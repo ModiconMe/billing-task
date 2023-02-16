0c325f6ff0a23f8bbf239930f34241203cf1bc13
saeed <rebeelsc@gmail.com>
2023-02-16 17:45:09 +0700

    add basic package structure

---
+++ src/main/java/io/modicon/taskapp/TaskApplication.java
@@ -0,0 +1,13 @@
+package io.modicon.taskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class, args);
+    }
+
+}
