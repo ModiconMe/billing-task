811849f1c6c14d8ab26898dafcc3c3835b5e4c8f
saeed <rebeelsc@gmail.com>
2023-02-18 23:10:01 +0700

    add tag repository tests

--- src/test/java/io/modicon/taskapp/domain/repository/TagRepositoryTest.java
+++ src/test/java/io/modicon/taskapp/domain/repository/TagRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,5 +35,64 @@ class TagRepositoryTest {
         assertTrue(result.contains(tag2));
         assertTrue(result.contains(tag3));
         assertFalse(result.contains(tag4));
+    }
+
+    @Test
+    void shouldFindByTagName() {
+        // given
+        TagEntity tag1 = new TagEntity("id1","tag1", 0L);
+        TagEntity tag2 = new TagEntity("id2","tag2", 2L);
+        tagRepository.save(tag1);
+        tagRepository.save(tag2);
+
+        // when
+        Optional<TagEntity> actual = tagRepository.findByTagName(tag1.getTagName());
+
+        // then
+        assertTrue(actual.isPresent());
+        assertEquals(tag1, actual.get());
+    }
+
+    @Test
+    void shouldNotFindByTagName() {
+        // given
+        TagEntity tag1 = new TagEntity("id1","tag1", 0L);
+        tagRepository.save(tag1);
+
+        // when
+        Optional<TagEntity> actual = tagRepository.findByTagName("wrongName");
+
+        // then
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    void shouldExistsByTagName() {
+        // given
+        TagEntity tag1 = new TagEntity("id1","tag1", 0L);
+        TagEntity tag2 = new TagEntity("id2","tag2", 2L);
+        tagRepository.save(tag1);
+        tagRepository.save(tag2);
+
+        // when
+        boolean actual = tagRepository.existsByTagName(tag1.getTagName());
+
+        // then
+        assertTrue(actual);
+    }
+
+    @Test
+    void shouldNotExistsByTagName() {
+        // given
+        TagEntity tag1 = new TagEntity("id1","tag1", 0L);
+        TagEntity tag2 = new TagEntity("id2","tag2", 2L);
+        tagRepository.save(tag1);
+        tagRepository.save(tag2);
+
+        // when
+        boolean actual = tagRepository.existsByTagName("wrong name");
+
+        // then
+        assertFalse(actual);
     }
 }
